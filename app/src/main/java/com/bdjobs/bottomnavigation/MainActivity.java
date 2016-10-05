6b9a2606e2df4058dcbc43248beabf7ee3cb5ef0
Rubayet Hassan <rubayet.evan@gmail.com>
2016-10-05 16:39:01 +0600

    Initial commit

---
+++ app/src/main/java/com/bdjobs/bottomnavigation/MainActivity.java
@@ -0,0 +1,48 @@
+package com.bdjobs.bottomnavigation;
+
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+public class MainActivity extends AppCompatActivity {
+    android.app.FragmentManager manager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        manager = getFragmentManager();
+        FragmentA fa = new FragmentA();
+        android.app.FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragmentHolder,fa,"A");
+        transaction.commit();
+
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.homeTab) {
+                    FragmentA fa = new FragmentA();
+                    android.app.FragmentTransaction transaction = manager.beginTransaction();
+                    transaction.replace(R.id.fragmentHolder,fa,"A");
+                    transaction.commit();
+                }
+                else if (tabId == R.id.likeTab) {
+                    FragmentB fb = new FragmentB();
+                    android.app.FragmentTransaction transaction = manager.beginTransaction();
+                    transaction.replace(R.id.fragmentHolder,fb,"B");
+                    transaction.commit();
+                }
+                else if (tabId == R.id.shareTab) {
+                    FragmentC fc = new FragmentC();
+                    android.app.FragmentTransaction transaction = manager.beginTransaction();
+                    transaction.replace(R.id.fragmentHolder,fc,"A");
+                    transaction.commit();
+                }
+            }
+        });
+    }
+}
+
